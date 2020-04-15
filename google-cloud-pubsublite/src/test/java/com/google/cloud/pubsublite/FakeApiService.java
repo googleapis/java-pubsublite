1db6d4cceb85c25a5ef9432f4619568bbc64621c
Daniel Collins <dpcollins@google.com>
2020-04-15 11:32:28 -0400

    Add license to FakeApiService.

--- google-cloud-pubsublite/src/test/java/com/google/cloud/pubsublite/FakeApiService.java
+++ google-cloud-pubsublite/src/test/java/com/google/cloud/pubsublite/FakeApiService.java
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.cloud.pubsublite;
 
 import com.google.api.core.AbstractApiService;
@@ -5,8 +19,10 @@ import com.google.api.core.AbstractApiService;
 /**
  * Fake Pub/Sub Lite service for testing. Used like:
  *
- * <p>static abstract class SubscriberFakeService extends FakeApiService implements Subscriber
- * {} @Spy private SubscriberFakeService wireSubscriber;
+ * <p>
+ * static abstract class SubscriberFakeService extends FakeApiService implements Subscriber {}
+ * @Spy private SubscriberFakeService wireSubscriber;
+ * </p>
  */
 public abstract class FakeApiService extends AbstractApiService {
   public void fail(Throwable t) {

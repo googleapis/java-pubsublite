345b1e5963ec4120a1de7ef12c60af1fb6b1ec93
Jeff Ching <chingor@google.com>
2020-04-29 13:19:33 -0700

    chore: add common templates, fix dependency imports

---
+++ synth.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import synthtool.languages.java as java
+
+# TODO: generate library protos when publicly available
+
+java.common_templates(excludes=[
+  # TODO: allow README generation when public
+  'README.md',
+  # TODO: allow when pubsublite is available in libraries-bom
+  'samples/install-without-bom/*',
+])

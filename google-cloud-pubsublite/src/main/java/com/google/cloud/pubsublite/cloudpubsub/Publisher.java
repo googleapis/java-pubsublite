fc363489d7dbc6f699a89aa61db00c25e8ef09de
Daniel Collins <dpcollins@google.com>
2020-05-07 15:20:48 -0400

    Restructure publisher builder to use a setttings pattern instead.

--- google-cloud-pubsublite/src/main/java/com/google/cloud/pubsublite/cloudpubsub/Publisher.java
+++ google-cloud-pubsublite/src/main/java/com/google/cloud/pubsublite/cloudpubsub/Publisher.java
@@ -16,90 +16,13 @@
 
 package com.google.cloud.pubsublite.cloudpubsub;
 
-import com.google.api.gax.batching.BatchingSettings;
-import com.google.auto.value.AutoValue;
-import com.google.cloud.pubsublite.Constants;
-import com.google.cloud.pubsublite.Message;
-import com.google.cloud.pubsublite.MessageTransformer;
-import com.google.cloud.pubsublite.TopicPath;
-import com.google.cloud.pubsublite.cloudpubsub.internal.WrappingPublisher;
-import com.google.cloud.pubsublite.internal.wire.PubsubContext;
-import com.google.cloud.pubsublite.internal.wire.PubsubContext.Framework;
-import com.google.cloud.pubsublite.internal.wire.RoutingPublisherBuilder;
-import com.google.cloud.pubsublite.internal.wire.SinglePartitionPublisherBuilder;
-import com.google.cloud.pubsublite.proto.PublisherServiceGrpc.PublisherServiceStub;
-import com.google.pubsub.v1.PubsubMessage;
+import com.google.api.core.ApiService;
+import com.google.cloud.pubsub.v1.PublisherInterface;
 import io.grpc.StatusException;
-import java.util.Optional;
-import org.threeten.bp.Duration;
 
-@AutoValue
-public abstract class Publisher {
-  public static final BatchingSettings DEFAULT_BATCHING_SETTINGS =
-      BatchingSettings.newBuilder()
-          .setIsEnabled(true)
-          .setElementCountThreshold(1000L)
-          .setRequestByteThreshold(Constants.MAX_PUBLISH_BATCH_BYTES)
-          .setDelayThreshold(Duration.ofMillis(50))
-          .build();
-  private static final Framework FRAMEWORK = Framework.of("CLOUD_PUBSUB_SHIM");
-
-  // Required parameters.
-  abstract TopicPath topicPath();
-
-  // Optional parameters.
-  abstract Optional<KeyExtractor> keyExtractor();
-
-  abstract Optional<MessageTransformer<PubsubMessage, Message>> messageTransformer();
-
-  abstract Optional<BatchingSettings> batchingSettings();
-
-  abstract Optional<PublisherServiceStub> stub();
-
-  public static Builder newBuilder() {
-    return new AutoValue_Publisher.Builder();
-  }
-
-  @AutoValue.Builder
-  public abstract static class Builder {
-    // Required parameters.
-    public abstract Builder setTopicPath(TopicPath path);
-
-    // Optional parameters.
-    public abstract Builder setKeyExtractor(KeyExtractor keyExtractor);
-
-    public abstract Builder setMessageTransformer(
-        MessageTransformer<PubsubMessage, Message> messageTransformer);
-
-    public abstract Builder setBatchingSettings(BatchingSettings batchingSettings);
-
-    public abstract Builder setStub(PublisherServiceStub stub);
-
-    abstract Publisher autoBuild();
-
-    @SuppressWarnings("CheckReturnValue")
-    public PublisherApiService build() throws StatusException {
-      Publisher builder = autoBuild();
-      BatchingSettings batchingSettings =
-          builder.batchingSettings().orElse(DEFAULT_BATCHING_SETTINGS);
-      KeyExtractor keyExtractor = builder.keyExtractor().orElse(KeyExtractor.DEFAULT);
-      MessageTransformer<PubsubMessage, Message> messageTransformer =
-          builder
-              .messageTransformer()
-              .orElseGet(() -> MessageTransforms.fromCpsPublishTransformer(keyExtractor));
-
-      SinglePartitionPublisherBuilder.Builder singlePartitionPublisherBuilder =
-          SinglePartitionPublisherBuilder.newBuilder()
-              .setBatchingSettings(Optional.of(batchingSettings))
-              .setStub(builder.stub())
-              .setContext(PubsubContext.of(FRAMEWORK));
-
-      RoutingPublisherBuilder.Builder wireBuilder =
-          RoutingPublisherBuilder.newBuilder()
-              .setTopic(builder.topicPath())
-              .setPublisherBuilder(singlePartitionPublisherBuilder);
-
-      return new WrappingPublisher(wireBuilder.build(), messageTransformer);
-    }
+// Pub/Sub Lite publishers are streaming based and can experience permanent errors.
+public interface Publisher extends PublisherInterface, ApiService {
+  static Publisher create(PublisherSettings settings) throws StatusException {
+    return settings.instantiate();
   }
 }

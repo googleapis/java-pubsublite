4d48ac3f58f8d1d8fced400d2fbba58c5859587a
dpcollins-google <40498610+dpcollins-google@users.noreply.github.com>
2020-06-15 16:01:27 -0400

    [feat] Implement ConnectedAssignerImpl and AssignerImpl which handle interactions with the assignment stream. (#134)

--- google-cloud-pubsublite/src/test/java/com/google/cloud/pubsublite/internal/wire/AssignerImplTest.java
+++ google-cloud-pubsublite/src/test/java/com/google/cloud/pubsublite/internal/wire/AssignerImplTest.java
@@ -17,27 +17,35 @@
 package com.google.cloud.pubsublite.internal.wire;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import com.google.api.core.ApiService.Listener;
 import com.google.cloud.pubsublite.CloudRegion;
 import com.google.cloud.pubsublite.CloudZone;
+import com.google.cloud.pubsublite.Partition;
 import com.google.cloud.pubsublite.ProjectNumber;
 import com.google.cloud.pubsublite.SubscriptionName;
 import com.google.cloud.pubsublite.SubscriptionPaths;
-import com.google.cloud.pubsublite.internal.FakeApiService;
+import com.google.cloud.pubsublite.internal.StatusExceptionMatcher;
 import com.google.cloud.pubsublite.proto.InitialPartitionAssignmentRequest;
 import com.google.cloud.pubsublite.proto.PartitionAssignment;
 import com.google.cloud.pubsublite.proto.PartitionAssignmentRequest;
 import com.google.cloud.pubsublite.proto.PartitionAssignmentServiceGrpc;
 import com.google.cloud.pubsublite.proto.PartitionAssignmentServiceGrpc.PartitionAssignmentServiceStub;
+import com.google.common.util.concurrent.MoreExecutors;
 import io.grpc.ManagedChannel;
+import io.grpc.Status;
+import io.grpc.Status.Code;
 import io.grpc.StatusException;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.stub.StreamObserver;
 import io.grpc.testing.GrpcCleanupRule;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -45,7 +53,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
 @RunWith(JUnit4.class)
 public class AssignerImplTest {
@@ -69,10 +76,18 @@ public class AssignerImplTest {
     }
   }
 
-  abstract static class ConnectedAssignerFakeService extends FakeApiService
-      implements ConnectedAssigner {}
+  private static final PartitionAssignment ASSIGNMENT =
+      PartitionAssignment.newBuilder().addPartitions(1).addPartitions(7).build();
 
-  @Spy private ConnectedAssignerFakeService connectedAssigner;
+  private Set<Partition> partitions() throws StatusException {
+    Set<Partition> partitions = new HashSet<>();
+    for (long partition : ASSIGNMENT.getPartitionsList()) {
+      partitions.add(Partition.of(partition));
+    }
+    return partitions;
+  }
+
+  @Mock private ConnectedAssigner connectedAssigner;
   @Mock private ConnectedAssignerFactory assignerFactory;
 
   @Mock private PartitionAssignmentReceiver receiver;
@@ -97,11 +112,27 @@ public class AssignerImplTest {
     PartitionAssignmentServiceStub unusedStub = PartitionAssignmentServiceGrpc.newStub(channel);
     assigner =
         new AssignerImpl(unusedStub, assignerFactory, initialRequest().getInitial(), receiver);
+    assigner.addListener(permanentErrorHandler, MoreExecutors.directExecutor());
+    assigner.startAsync().awaitRunning();
+    verify(assignerFactory).New(any(), any(), eq(initialRequest()));
   }
 
   @Test
   public void construct_CallsFactoryNew() {
     verifyNoMoreInteractions(assignerFactory);
     verifyNoMoreInteractions(connectedAssigner);
+  }
+
+  @Test
+  public void delivery_CallsReceiver() throws StatusException {
+    leakedResponseObserver.onNext(ASSIGNMENT);
+    verify(receiver).handleAssignment(partitions());
+  }
+
+  @Test
+  public void responseObserverFailure_Fails() {
+    leakedResponseObserver.onError(Status.INVALID_ARGUMENT.asException());
+    verify(permanentErrorHandler)
+        .failed(any(), argThat(new StatusExceptionMatcher(Code.INVALID_ARGUMENT)));
   }
 }

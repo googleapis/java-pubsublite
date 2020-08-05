1ca49567e9aadfd2faede1d9a1c7e508f4366910
Daniel Collins <dpcollins@google.com>
2020-08-05 12:30:41 -0400

    fix: Implement DefaultRoutingPolicyTest, which has a fixed set of test cases to ensure that all client libraries route the same keys to the same partitions.

---
+++ google-cloud-pubsublite/src/test/java/com/google/cloud/pubsublite/internal/DefaultRoutingPolicyTest.java
@@ -0,0 +1,46 @@
+package com.google.cloud.pubsublite.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.pubsublite.Partition;
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.protobuf.ByteString;
+import io.grpc.StatusException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.function.Supplier;
+import org.junit.Test;
+
+public class DefaultRoutingPolicyTest {
+  private static final int NUM_PARTITIONS = 29;
+  private static final RoutingPolicy policy = ((Supplier<RoutingPolicy>)() -> {
+    try {
+      return new DefaultRoutingPolicy(NUM_PARTITIONS);
+    } catch (StatusException e) {
+      throw e.getStatus().asRuntimeException();
+    }
+  }).get();
+
+  private static Map<ByteString, Partition> loadTestCases() throws Exception {
+    Gson gson = new Gson();
+    String json = new String(Files.readAllBytes(Paths.get(DefaultRoutingPolicyTest.class.getResource("/routing_tests.json").toURI())));
+    Map<String, Double> map = gson.fromJson(json, Map.class);
+    ImmutableMap.Builder<ByteString, Partition> output = ImmutableMap.builder();
+    for (String key : map.keySet()) {
+      output.put(ByteString.copyFromUtf8(key), Partition.of((int)map.get(key).doubleValue()));
+    }
+    return output.build();
+  }
+
+  @Test
+  public void routingPerformedCorrectly() throws Exception {
+    Map<ByteString, Partition> map = loadTestCases();
+    ImmutableMap.Builder<ByteString, Partition> results = ImmutableMap.builder();
+    for (ByteString key : map.keySet()) {
+      results.put(key, policy.route(key));
+    }
+    assertThat(results.build()).containsExactlyEntriesIn(map);
+  }
+}

12dd8990c7888c05d5a7d16775f73c0a9213f471
Tianzi Cai <tianzi@google.com>
2021-06-14 09:53:20 -0700

    revert changes in synth.py

--- synth.py
+++ synth.py
@@ -29,5 +29,4 @@ java.common_templates(excludes=[
   '.kokoro/*/samples.cfg',
   # TODO: allow when pubsublite is available in libraries-bom
   'samples/install-without-bom/*',
-  '.github/CODEOWNERS',
 ])

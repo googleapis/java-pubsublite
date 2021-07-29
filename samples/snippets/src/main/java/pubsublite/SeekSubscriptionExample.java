d8bcd0c2a384b229c4b4bb0e4cda73623467c57c
tmdiep <tmdiep@google.com>
2021-07-29 19:35:13 -0400

    Add comments about options. Remove beta api warning.

--- samples/snippets/src/main/java/pubsublite/SeekSubscriptionExample.java
+++ samples/snippets/src/main/java/pubsublite/SeekSubscriptionExample.java
@@ -40,8 +40,19 @@ public class SeekSubscriptionExample {
     String subscriptionId = "your-subscription-id";
     long projectNumber = Long.parseLong("123456789");
 
-    // Optional parameters.
+    // Choose a target location within the message backlog to seek a subscription to.
+    // Possible values for SeekTarget:
+    // - `SeekTarget.of(BacklogLocation.BEGINNING)`: replays from the beginning of all retained
+    //    messages.
+    // - `SeekTarget.of(BacklogLocation.END)`: skips past all current published messages.
+    // - `SeekTarget.ofPublishTime(<timestamp>)`: delivers messages with publish time greater than
+    //    or equal to the specified timestamp.
+    // - `SeekTarget.ofEventTime(<timestamp>)`: seeks to the first message with event time greater
+    //    than or equal to the specified timestamp.
     SeekTarget target = SeekTarget.of(BacklogLocation.BEGINNING);
+
+    // Optional: Wait for the seek operation to complete, which indicates when subscribers for all
+    // partitions are receiving messages from the seek target.
     boolean waitForOperation = false;
 
     seekSubscriptionExample(
@@ -68,12 +79,14 @@ public class SeekSubscriptionExample {
         AdminClientSettings.newBuilder().setRegion(CloudRegion.of(cloudRegion)).build();
 
     try (AdminClient adminClient = AdminClient.create(adminClientSettings)) {
+      // Initiate an out-of-band seek for a subscription to the specified target. If an operation is
+      // returned, the seek has been successfully registered and will eventually complete.
       OperationFuture<SeekSubscriptionResponse, OperationMetadata> seekFuture =
           adminClient.seekSubscription(subscriptionPath, target);
       System.out.println("Seek operation " + seekFuture.getName() + " initiated successfully.");
 
-      // Optional: Wait for the seek operation to complete, which indicates when subscribers for all
-      // partitions are receiving messages from the seek target.
+      // Optionally wait for the seek to propagate to subscribers. If subscribers are offline, the
+      // seek will complete once they are online.
       if (waitForOperation) {
         System.out.println("Waiting for operation to complete...");
         seekFuture.get();

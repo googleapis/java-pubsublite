2429c769dbe2ee950e1a10253f7b6de79fe0be62
Daniel Collins <dpcollins@google.com>
2022-12-08 09:40:12 -0500

    feat: unload partitions when not publishing for better performance

---
+++ google-cloud-pubsublite/src/main/java/com/google/cloud/pubsublite/internal/wire/UnloadingPublisher.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.pubsublite.internal.wire;
+
+import com.google.api.core.AbstractApiService;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.pubsublite.Message;
+import com.google.cloud.pubsublite.MessageMetadata;
+import com.google.cloud.pubsublite.internal.AlarmFactory;
+import com.google.cloud.pubsublite.internal.Publisher;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.concurrent.Future;
+import java.util.function.Supplier;
+import javax.annotation.concurrent.GuardedBy;
+
+/** A publisher which tears down connections when inactive. */
+class UnloadingPublisher extends AbstractApiService implements Publisher<MessageMetadata> {
+  private final Supplier<Publisher<MessageMetadata>> supplier;
+  private final Future<?> alarmFuture;
+
+  @GuardedBy("this")
+  private Optional<Publisher<MessageMetadata>> publisher = Optional.empty();
+
+  @GuardedBy("this")
+  private boolean sawPublish = false;
+
+  UnloadingPublisher(Supplier<Publisher<MessageMetadata>> supplier, AlarmFactory unloadAlarm) {
+    this.supplier = supplier;
+    this.alarmFuture = unloadAlarm.newAlarm(this::onUnloadAlarm);
+  }
+
+  @Override
+  protected void doStart() {
+    notifyStarted();
+  }
+
+  @Override
+  protected synchronized void doStop() {
+    alarmFuture.cancel(false);
+    if (!publisher.isPresent()) {
+      notifyStopped();
+    }
+    publisher
+        .get()
+        .addListener(
+            new Listener() {
+              @Override
+              public void terminated(State from) {
+                notifyStopped();
+              }
+            },
+            SystemExecutors.getFuturesExecutor());
+    publisher.get().stopAsync();
+  }
+
+  @Override
+  public synchronized ApiFuture<MessageMetadata> publish(Message message) {
+    sawPublish = true;
+    return getPublisher().publish(message);
+  }
+
+  private synchronized Publisher<MessageMetadata> getPublisher() {
+    if (!publisher.isPresent()) {
+      publisher = Optional.of(supplier.get());
+      publisher
+          .get()
+          .addListener(
+              new Listener() {
+                @Override
+                public void failed(State from, Throwable failure) {
+                  notifyFailed(failure);
+                }
+              },
+              SystemExecutors.getFuturesExecutor());
+      publisher.get().startAsync().awaitRunning();
+    }
+    return publisher.get();
+  }
+
+  @Override
+  public synchronized void cancelOutstandingPublishes() {
+    if (publisher.isPresent()) {
+      publisher.get().cancelOutstandingPublishes();
+    }
+  }
+
+  @Override
+  public synchronized void flush() throws IOException {
+    if (publisher.isPresent()) {
+      publisher.get().flush();
+    }
+  }
+
+  private synchronized void onUnloadAlarm() {
+    if (publisher.isPresent() && !sawPublish) {
+      publisher.get().stopAsync().awaitTerminated();
+      publisher = Optional.empty();
+    }
+    sawPublish = false;
+  }
+}
--- google-cloud-pubsublite/src/test/java/com/google/cloud/pubsublite/internal/wire/SinglePartitionPublisherTest.java
+++ google-cloud-pubsublite/src/test/java/com/google/cloud/pubsublite/internal/wire/SinglePartitionPublisherTest.java
@@ -17,8 +17,6 @@
 package com.google.cloud.pubsublite.internal.wire;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.RETURNS_SELF;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,25 +24,17 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 import com.google.api.core.ApiFuture;
 import com.google.api.core.SettableApiFuture;
-import com.google.api.gax.batching.BatchingSettings;
-import com.google.cloud.pubsublite.CloudRegion;
-import com.google.cloud.pubsublite.CloudZone;
 import com.google.cloud.pubsublite.Message;
 import com.google.cloud.pubsublite.MessageMetadata;
 import com.google.cloud.pubsublite.Offset;
 import com.google.cloud.pubsublite.Partition;
-import com.google.cloud.pubsublite.ProjectNumber;
-import com.google.cloud.pubsublite.TopicName;
-import com.google.cloud.pubsublite.TopicPath;
 import com.google.cloud.pubsublite.internal.Publisher;
 import com.google.cloud.pubsublite.internal.testing.FakeApiService;
-import com.google.cloud.pubsublite.internal.wire.StreamFactories.PublishStreamFactory;
 import com.google.protobuf.ByteString;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mock;
 import org.mockito.Spy;
 
 @RunWith(JUnit4.class)
@@ -52,34 +42,14 @@ public class SinglePartitionPublisherTest {
   abstract static class FakeOffsetPublisher extends FakeApiService implements Publisher<Offset> {}
 
   @Spy private FakeOffsetPublisher underlying;
-  @Mock private PublishStreamFactory streamFactory;
 
   private Publisher<MessageMetadata> pub;
 
   @Before
   public void setUp() {
     initMocks(this);
-
-    TopicPath topic =
-        TopicPath.newBuilder()
-            .setName(TopicName.of("abc"))
-            .setProject(ProjectNumber.of(123))
-            .setLocation(CloudZone.of(CloudRegion.of("us-central1"), 'a'))
-            .build();
     Partition partition = Partition.of(3);
-
-    PublisherBuilder.Builder mockBuilder = mock(PublisherBuilder.Builder.class, RETURNS_SELF);
-    when(mockBuilder.build()).thenReturn(underlying);
-
-    when(mockBuilder.setTopic(topic)).thenReturn(mockBuilder);
-    this.pub =
-        SinglePartitionPublisherBuilder.newBuilder()
-            .setTopic(topic)
-            .setPartition(partition)
-            .setUnderlyingBuilder(mockBuilder)
-            .setBatchingSettings(BatchingSettings.newBuilder().setIsEnabled(false).build())
-            .setStreamFactory(streamFactory)
-            .build();
+    this.pub = new SinglePartitionPublisher(underlying, partition);
     this.pub.startAsync().awaitRunning();
   }
 
